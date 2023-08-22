8ab006abb609c000421938d23b562eb6206e3452
eddy <eddytech03@gmail.com>
2023-08-22 22:00:11 +0800

    using default port 4043

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const next = require("next");
 const _ = require("lodash");
 
 const dev = process.env.NODE_ENV !== "production";
-const port = Number(process.env.PORT || 3000);
+const port = Number(process.env.PORT || 4043);
 
 const app = next({dev, hostname: '0.0.0.0', port});
 const handle = app.getRequestHandler();
